0e6474896d47aa386b75677aa259b5864110bbcb
Michael Prather <mprather1@gmail.com>
2018-02-12 03:43:24 -0600

    update

--- client.sh
+++ client.sh
@@ -1,3 +1,6 @@
+# hostname fqdn
+# edit /etc/hostname
+
 sudo apt-get update && \
 
 sudo apt-get upgrade -y && \
@@ -8,6 +11,8 @@ sudo ipa-client-install --mkhomedir && \
 
 sudo systemctl enable oddjob && \
 sudo systemctl start oddjob && \
+
+# edit /etc/hosts 127.0.1.1 hostname.freeipa-domain-name hostname
 
 sudo bash -c "cat > /usr/share/pam-configs/mkhomedir" <<EOF
 Name: activate mkhomedir
