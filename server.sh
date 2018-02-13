ce98b2ded7e62ce0d0ab6daa1134415805d575c2
Michael Prather <mprather1@users.noreply.github.com>
2018-02-13 00:21:28 -0600

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,12 +1,17 @@
 # hostname example.com
+# edit /etc hosts
 
 yum update -y && \
 yum install firewalld bind-utils rng-tools -y && \
+
+systemctl start firewalld && \
+
 firewall-cmd --permanent --add-port={80/tcp,443/tcp,389/tcp,636/tcp,88/tcp,464/tcp,53/tcp,88/udp,464/udp,53/udp,123/udp} && \
 firewall-cmd --reload && \
 
 # nano /etc/hosts ip6 ip4
-# nano /etc/cloud/templates/hosts.redhat.tmpl
+# ip example.com example.com
+# nano /etc/cloud/templates/hosts.redhat.tmpl with ips
 
 systemctl start rngd && \
 systemctl enable rngd && \
